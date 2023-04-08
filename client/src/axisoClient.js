94de4bd6b909b0d4837e4e22c761eabf827cbee4
Jatin Kushwah <singhjatin354@gmail.com>
2023-04-08 07:49:51 +0530

    deployment url updated

--- client/src/axisoClient.js
+++ client/src/axisoClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: process.env.REACT_APP_SERVER_BASE_URL || "http://localhost:4000",
 });
 
 export default axiosClient;
--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "http://localhost:3000" || process.env.CORS_ORIGIN,
     })
 );
 
